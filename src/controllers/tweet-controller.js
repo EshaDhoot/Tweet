6fc59092bdc079e07b0e415beb08bdbdc98ab259
Esha <maheshwariesha11@gmail.com>
2024-02-12 12:50:00 +0530

    Added get tweet with comments api

--- src/controllers/tweet-controller.js
+++ src/controllers/tweet-controller.js
@@ -20,3 +20,22 @@ export const createTweet = async(req, res) => {
         })
     }
 }
+
+export const getTweet = async(req, res) => {
+    try {
+        const response = await tweetService.get(req.query.id);
+        return res.status(200).json({
+            success: true,
+            message: 'Successfully fetched a tweet',
+            err: {},
+            data: response
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Unable to fetch a tweet',
+            err: error,
+            data: {} 
+        })
+    }
+}
